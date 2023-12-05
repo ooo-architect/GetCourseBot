c7764e45a00dd723cdcd82051ef69bdd51a5eea3
Mihail Butvin <butvin.mihail@yandex.ru>
2023-12-05 17:45:55 +0300

    Proper handling of many member accounts

--- BotMicro/bot/handlers/groups/members_control.py
+++ BotMicro/bot/handlers/groups/members_control.py
@@ -1,23 +1,31 @@
-from datetime import datetime
+from typing import Optional
 
 from aiogram import Bot, Router
 from aiogram.filters import IS_MEMBER, IS_NOT_MEMBER, ChatMemberUpdatedFilter
 from aiogram.types import ChatMemberUpdated, Message
-from deta import Deta
-from deta._async.client import _AsyncBase
 
 from models.group import Group
 from models.member import Member
 
+from utils.logging import log_to_deta
+
 router = Router()
 
 
-async def remove_member(bot: Bot, chat_id: int, user_id: int):
+async def remove_member(
+    bot: Bot,
+    chat_id: int,
+    user_id: int,
+    member: Optional[Member],
+):
     # ban and unban to remove user from group but leave ability of come back
     await bot.ban_chat_member(chat_id, user_id, revoke_messages=True)
     await bot.unban_chat_member(chat_id, user_id)
-    base: _AsyncBase = Deta().AsyncBase('MockRemoveLogs')
-    await base.put({'time': datetime.now().isoformat(), 'chat_id': chat_id, 'user_id': user_id})
+    await log_to_deta({
+        'member': member.dict() if member else None,
+        'user_id': user_id,
+        'chat_id': chat_id,
+    })
 
 
 @router.message()
@@ -30,18 +38,20 @@ async def member_message_handler(message: Message, bot: Bot):
 
     members = await Member.query(Member.user_id == user_id)
     if not members:
-        return await remove_member(bot, chat_id, user_id)
-
-    member = members.pop()
+        return await remove_member(bot, chat_id, user_id, None)
 
     groups = await Group.get_all()
     connected_groups = [
-        group for group in groups if chat_id in group.connected_chats]
-    for group in connected_groups:
-        if member.uid in group.unchecked_members:
-            return
+        group for group in groups
+        if chat_id in group.connected_chats
+    ]
 
-    await remove_member(bot, chat_id, user_id)
+    for member in members:
+        for group in connected_groups:
+            if member.uid in group.unchecked_members:
+                return
+
+    await remove_member(bot, chat_id, user_id, member)
 
 
 @router.chat_member(ChatMemberUpdatedFilter(IS_NOT_MEMBER >> IS_MEMBER))
@@ -51,15 +61,17 @@ async def new_member_handler(event: ChatMemberUpdated, bot: Bot):
 
     members = await Member.query(Member.user_id == user_id)
     if not members:
-        return await remove_member(bot, chat_id, user_id)
-
-    member = members.pop()
+        return await remove_member(bot, chat_id, user_id, None)
 
     groups = await Group.get_all()
     connected_groups = [
-        group for group in groups if chat_id in group.connected_chats]
-    for group in connected_groups:
-        if member.uid in group.unchecked_members:
-            return
+        group for group in groups
+        if chat_id in group.connected_chats
+    ]
 
-    await remove_member(bot, chat_id, user_id)
+    for member in members:
+        for group in connected_groups:
+            if member.uid in group.unchecked_members:
+                return
+
+    await remove_member(bot, chat_id, user_id, member)
